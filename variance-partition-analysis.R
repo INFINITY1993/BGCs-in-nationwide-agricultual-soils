57dcaea223fe25690146c6943aa62f21f34a2f37
Zhiguo ZHANG <63569349+INFINITY1993@users.noreply.github.com>
2023-04-07 09:25:58 +0800

    Update variance-partition-analysis.R

--- variance-partition-analysis.R
+++ variance-partition-analysis.R
@@ -5,25 +5,25 @@ library(dplyr)
 library(SoDA)
 
 
-#VPA分析就是采用的partial RDA原理，两者计算的结果是一致的
+#VPA analysis uses the partial RDA principle, and the results of the two calculations are consistent
 
-#读取文件
+#Read file
 gcf_abundance <- read.csv("gcf_abundance.csv",header = T,row.names = 1)
 environment_factor <- read.csv("environment.csv",header = T,row.names = 1)
 taxa <- read.csv("16s_phylum_abundance.csv",header = T,row.names = 1)
 taxa1 <- as.data.frame(t(taxa))
 taxa_dominant <-taxa1[,1:15]
 
-#按样本名排序
+#Sorted by sample name
 environment_factor_s <- environment_factor[order(rownames(environment_factor)),]
 gcf_abundance_s <- gcf_abundance[order(rownames(gcf_abundance)),]
 gcf_abundance_s.h <- decostand(gcf_abundance_s, "hellinger")
 taxa_s <- taxa_dominant[order(rownames(taxa_dominant)),]
 
-#环境因子数据标准化，均值为0，标准差为1
+#Data normalization of environmental variables，mean value 0，standard deviation 1
 env.scaled<-data.frame(scale(environment_factor_s,center = F)) #数据标准化，均值为0，标准差为1
 
-#经纬度转换pcnm
+# Coordinates to pcnm
 geo <- subset(environment_factor_s,select = c(longitude,latitude))
 geo.xy <- geoXY(geo$longitude, geo$latitude, unit = 1)
 pcnm1 <- pcnm(dist(geo.xy))
@@ -31,15 +31,11 @@ df_pcnm <- data.frame(pcnm1$vectors)
 rownames(df_pcnm) <- rownames(environment_factor)
 
 
-#在多因素的分析中，例如 RDA 分析和多元线性回归，
-#任何增加新的变量，都会使得模型看起来拟合得更好，然而实际情况显然并非如此，为了解决过拟合问题调整R方是一个很常用的指标
-
-#全模型逐渐消减变量
+#In multi-factor analysis, such as RDA analysis and multiple linear regression, any addition of new variables will make the model seem to fit better, but the actual situation is obviously not the case. In order to solve the overfitting problem, it is a common index to adjust R square
 # Create the full model
 rda_full <- rda(gcf_abundance_s.h~., data = env.scaled)
 rda_back <- ordistep(rda_full, direction = 'backward',trace = 0)
 
-#从零模型逐渐增加变量以及双向选择变量
 # Create the zero model
 rda_null <- rda(gcf_abundance_s.h~1, data = env.scaled)
 rda_frwd <- ordistep(rda_null, formula(rda_full), direction = 'forward',trace = 0)
@@ -49,10 +45,10 @@ rda_back
 rda_frwd
 rda_both
 
-##根据以上三个运行结果，选择筛选后的解释变量pH, MC,NO3,TN,DOC, AK, MAT,MAP
+#Based on the above results, pH, MC,NO3,TN,DOC, AK, MAT, MAP were selected
 
 
-##方差分解分析
+##Variance partition analysis
 df_clim <- env.scaled %>% select(MAT,MAP)
 df_soil <- env.scaled %>% select(pH, MC,NO3,TN,DOC, AK)
 df_geo <- cbind(df_clim,df_pcnm)
@@ -61,7 +57,7 @@ df_taxa <- taxa_s
 vpt <- varpart(gcf_abundance_s.h, df_geo,df_taxa, df_soil,chisquare=FALSE)
 vpt
 
-#绘制图表
+#Figure plot
 plot(
   vpt,
   bg = c("#6A6599FF","#DF8F44FF","#00A1D5FF"),
@@ -73,7 +69,7 @@ plot(
 
 
 
-#显著性检验
+#Significance test
 formula_soil <- formula(clfs_surface_s.h ~ as.matrix(df_soil) +Condition(as.matrix(df_geo))+Condition(as.matrix(df_taxa)))
 formula_pcnm_clim <- formula(clfs_surface_s.h ~ as.matrix(df_geo) +Condition(as.matrix(df_soil))+Condition(as.matrix(df_taxa)))
 formula_taxa <- formula(clfs_surface_s.h ~ as.matrix(df_taxa) +Condition(as.matrix(df_geo))+Condition(as.matrix(df_soil)))
